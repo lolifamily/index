a9a24dd40513e01be2438feab33167648bb89a70
lolifamily <lolifamilies@gmail.com>
2023-06-24 21:39:59 +0800

    Update

--- pages/api/css.tsx
+++ pages/api/css.tsx
@@ -17,7 +17,7 @@ export default async function css(req: NextRequest) {
 
     headers = new Headers();
     for (let item of res.headers.entries()) {
-        if (item[0] === "content-encoding" || item[0] === "content-length") continue;
+        if (item[0] === "content-encoding" || item[0] === "content-length" || item[0] === "Alt-Svc") continue;
         headers.set(item[0], item[1].replaceAll("fonts.googleapis.com", req.nextUrl.host));
     }
 
