70db941c91db075515cd250f798114920e1a66eb
lolifamily <lolifamilies@gmail.com>
2023-06-24 21:35:12 +0800

    Init
    Signed-off-by: lolifamily <lolifamilies@gmail.com>

---
+++ middleware.tsx
@@ -0,0 +1,12 @@
+import {NextRequest, NextResponse} from "next/server";
+import {next} from "sucrase/dist/types/parser/tokenizer";
+
+const allowList = ["lolifamily.js.org", "lolifamily.vercel.app", "lolifamilies.vercel.app",
+    "lolistation.vercel.app", "lolistations.vercel.app"];
+
+export function middleware(req: NextRequest) {
+    if (allowList.indexOf(req.nextUrl.host) === -1) {
+        return NextResponse.redirect("https://lolifamily.js.org");
+    }
+    return NextResponse.next();
+}
---
+++ pages/api/css.tsx
@@ -0,0 +1,29 @@
+import {NextRequest, NextResponse} from "next/server";
+
+export const config = {
+    runtime: 'experimental-edge'
+};
+
+export default async function css(req: NextRequest) {
+    let headers = new Headers();
+    for (let item of headers.entries()) {
+        if (item[0].startsWith("x-")) continue;
+        headers.set(item[0], item[1].replaceAll(req.nextUrl.host, "fonts.googleapis.com"));
+    }
+
+    const res = await fetch("https://fonts.googleapis.com/css?family=Source+Sans+Pro:300", {
+        "headers": headers
+    });
+
+    headers = new Headers();
+    for (let item of res.headers.entries()) {
+        if (item[0] === "content-encoding" || item[0] === "content-length") continue;
+        headers.set(item[0], item[1].replaceAll("fonts.googleapis.com", req.nextUrl.host));
+    }
+
+    return new NextResponse((await res.text()).replaceAll("fonts.googleapis.com", req.nextUrl.host), {
+        "status": res.status,
+        "statusText": res.statusText,
+        "headers": headers
+    });
+}
