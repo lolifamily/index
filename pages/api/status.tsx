9d9d987d8ecccbe3e9d79f1e2714db195da8d30b
lolifamily <lolifamilies@gmail.com>
2023-06-25 15:53:00 +0800

    Update

--- pages/api/status.tsx
+++ pages/api/status.tsx
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.setHeader('Cache-Control', 's-maxage=86400');
     res.status(200).end("OK");
 }
