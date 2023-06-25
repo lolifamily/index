090518f096a7a2d46a118e165644f3b0ffe1705a
lolifamily <lolifamilies@gmail.com>
2023-06-25 09:38:28 +0800

    Update

--- pages/api/css.tsx
+++ pages/api/css.tsx
@@ -1,7 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 
 export const config = {
-    runtime: 'experimental-edge'
+    runtime: 'edge'
 };
 
 export default async function css(req: NextRequest) {
@@ -18,8 +18,9 @@ export default async function css(req: NextRequest) {
 
     headers = new Headers();
     for (let item of res.headers.entries()) {
-        if (item[0] === "content-encoding" || item[0] === "content-length" || item[0] === "Alt-Svc") continue;
+        if (item[0] === "content-encoding" || item[0] === "content-length" || item[0] === "alt-svc") continue;
         headers.set(item[0], item[1].replaceAll("fonts.googleapis.com", req.nextUrl.host));
+        console.log(item[0], item[1].replaceAll("fonts.googleapis.com", req.nextUrl.host));
     }
 
     return new NextResponse((await res.text()).replaceAll("fonts.googleapis.com", req.nextUrl.host), {
