b91dbca71cdd640549af833295889d675c53e06c
lolifamily <lolifamilies@gmail.com>
2023-06-25 15:30:19 +0800

    Update

--- pages/api/release.tsx
+++ pages/api/release.tsx
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    let env = process.env.VERCEL_ENV || "unknown";
-    let sha = process.env.VERCEL_GIT_COMMIT_SHA || "000000";
+    let env = req.env.VERCEL_ENV || "unknown";
+    let sha = req.env.VERCEL_GIT_COMMIT_SHA || "000000";
     res.status(200).end(env + ':' + sha.substring(0, 6));
 }
