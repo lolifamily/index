dd907b6791f821574a3dfacae14fd11b1a562a73
lolifamily <lolifamilies@gmail.com>
2023-09-19 13:49:05 +0800

    Update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    reactStrictMode: true,
+    skipTrailingSlashRedirect: true,
+    async headers() {
+        return [{
+            "source": "/(.*)",
+            "headers": [{
+                "key": "strict-transport-security",
+                "value": "max-age=63072000; includeSubDomains; preload"
+            },{
+                "key": "Content-Security-Policy",
+                "value": "default-src 'self' 'unsafe-eval' 'unsafe-inline'"
+            }]
+        }]
+    },
+    async rewrites() {
+        return {
+            beforeFiles: [{
+                "source": "/s/sourcesanspro/:path*",
+                "destination": "https://fonts.gstatic.com/s/sourcesanspro/:path*"
+            },{
+                "source": "/(.*)",
+                "destination": "/$1"
+            }],
+            fallback: [{
+                "source": "/(.*)",
+                "destination": "/"
+            }]
+        }
+    },
+}
 
 module.exports = nextConfig
