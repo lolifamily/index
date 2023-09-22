264b5c9d07d33445125b8c8e5cae66b010ed4745
lolifamily <lolifamilies@gmail.com>
2023-09-22 22:09:17 +0800

    Update

--- next.config.js
+++ next.config.js
@@ -18,14 +18,14 @@ const nextConfig = {
     async rewrites() {
         return {
             beforeFiles: [{
-                "source": "/cdn-cgi/:path*",
-                "destination": "https://cf.lolifamily.js.org/non-exist-destination?path=:path*"
+                "source": "/cdn-cgi/:path(.*)",
+                "destination": "https://cf.lolifamily.js.org/non-exist-destination?path=:path"
             },{
-                "source": "/s/sourcesanspro/:path*",
-                "destination": "https://fonts.gstatic.com/s/sourcesanspro/:path*"
+                "source": "/s/sourcesanspro/:path(.*)",
+                "destination": "https://fonts.gstatic.com/s/sourcesanspro/:path"
             },{
-                "source": "/Font-Awesome@5/:path*",
-                "destination": "https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome@5/:path*"
+                "source": "/Font-Awesome@6/:path(.*)",
+                "destination": "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6/:path"
             },{
                 "source": "/:path((?!api/css).*)",
                 "destination": "https://cf.lolifamily.js.org/:path"
