196747d6b5367217f34501475ff4124b039719a5
lolifamily <lolifamilies@gmail.com>
2024-05-15 05:20:36 -0700

    Update

--- next.config.js
+++ next.config.js
@@ -32,9 +32,6 @@ const nextConfig = {
             },{
                 "source": "/Font-Awesome@6/:path(.*)",
                 "destination": "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6/:path"
-            },{
-                "source": "/:path(.*)",
-                "destination": "/:path"
             }]
         }
     },
